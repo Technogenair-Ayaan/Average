080858cd45806f0c8128745583e3be1bc407e6f5
Ayaan <75617112+Technogenair-Ayaan@users.noreply.github.com>
2020-12-11 16:55:37 +0530

    Python Project
    This is a python project which finds out the average of two numbers.

---
+++ Average.py
@@ -0,0 +1,9 @@
+print("WELCOME TO AVERAGE CALCULATOR")
+print("Designed and Programmed by Ayaan Ansari")
+print("Type the first number:")
+var1 = int(input())
+print("Type your second number:")
+var2 = int(input())
+sum = int(var1) + int(var2)
+mean = int(sum) / 2
+print("The Average is",mean)
